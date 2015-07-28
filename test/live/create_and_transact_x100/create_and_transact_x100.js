4b3253782fbc38f14caaae8af64b0822df6285ba
androlo <andreas@erisindustries.com>
2015-07-28 23:21:14 +0200

    cleanup, 0.10.17

--- test/live/create_and_transact_x100/create_and_transact_x100.js
+++ test/live/create_and_transact_x100/create_and_transact_x100.js
@@ -13,7 +13,7 @@ var test_data = require('eris-db/test/testdata/testdata.json');
 var requestData = {
     priv_validator: test_data.chain_data.priv_validator,
     genesis: test_data.chain_data.genesis,
-    max_duration: 10
+    max_duration: 30
 };
 
 var abi = [
@@ -50,52 +50,61 @@ var privKey = "6B72D45EB65F619F11CE580C8CAED9E0BADC774E9C9C334687A65DCBAD2C4151C
 
 var contracts;
 var contract;
+var contractFactory;
 
-describe('TestCreateAndTransact', function () {
+describe('TestCreateAndTransact100Times', function () {
 
     before(function (done) {
         this.timeout(5000);
-        util.getNewErisServer(serverServerURL, requestData, function (err, port) {
-            if (err) {
-                throw err;
+        var edb = edbModule.createInstance("http://localhost:1337/rpc");
+        edb.start(function (error) {
+            if (error) {
+                throw error;
             }
-            var edb = edbModule.createInstance("ws://localhost:" + port + '/socketrpc');
-            edb.start(function(error){
-                if(error){
-                    throw error
+            var pipe = new eris.pipes.DevPipe(edb, privKey);
+            contracts = eris.contracts(pipe);
+            console.log("Creating. This should take a couple of seconds.");
+            contractFactory = contracts(abi);
+            contractFactory.new({data: code}, function (error, data) {
+                if (error) {
+                    console.log("New contract error");
+                    console.log(error);
+                    throw error;
                 }
-                var pipe = new eris.pipes.DevPipe(edb, privKey);
-                contracts = eris.contracts(pipe);
-                console.log("Creating. This should take a couple of seconds.");
-                var contractFactory = contracts(abi);
-                contractFactory.new({data: code}, function (error, data) {
-                    if (error) {
-                        console.log("New contract error");
-                        console.log(error);
-                        throw error;
-                    }
-                    contract = data;
-                    done();
-                });
+                contract = data;
+                done();
             });
-        })
+        });
     });
 
     describe('setTestBytes32', function () {
         var input = 0xDEADBEEF;
         var expected = ['0000000000000000000000000000000000000000000000000123456789ABCDEF',
             '00000000000000000000000000000000000000000000000000000000DEADBEEF'];
-        this.timeout(5000);
-        it("should set the bytes in the contract to 0xdeadbeef", function (done) {
-            contract.setTestBytes32(input, function (error, output) {
-                console.log("output");
-                console.log(output);
-                asrt.ifError(error);
-                asrt.deepEqual(output, expected);
-                done();
-            });
+
+        var expected2 = ['00000000000000000000000000000000000000000000000000000000DEADBEEF',
+            '00000000000000000000000000000000000000000000000000000000DEADBEEF'];
+
+        this.timeout(20000);
+
+        it("should set the bytes in the contract to 0xdeadbeef then do 100 transactions at once", function (done) {
+            for (var i = 0; i < 100; i++) {
+                contract.setTestBytes32(input, function (error, output) {
+                    asrt.ifError(error);
+                    reg(done);
+                });
+            }
         });
 
     });
+
+    var counter = 0;
+
+    function reg(done) {
+        if (++counter === 100) {
+            done();
+        }
+
+    }
 
 });
